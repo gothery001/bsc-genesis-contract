ef5ddc33d5c4908c641ca009fe061ea039da0a11
gothery001 <gothery001>
2022-04-25 13:44:38 +0800

    chores

--- scripts/6-info-maintenance.ts
+++ scripts/6-info-maintenance.ts
@@ -19,24 +19,9 @@ const init = async () => {
         'BSCValidatorSet',
         SYSTEM_CONTRACT_ADDRESS.ValidatorContract
     )) as BSCValidatorSet;
-    const validators = await bscValidatorSet.getValidators();
-    log('validators:', validators);
-    log('chainId:', await bscValidatorSet.bscChainID());
 
-
-
-    let index = await bscValidatorSet.getCurrentValidatorIndex('0x3f58fb8ec2f78b65a9077cd3c33b90f3e2332d95')
-    let info = await bscValidatorSet.currentValidatorSet(index)
-    let extraInfo = await bscValidatorSet.validatorExtraSet(index)
-    let isWorking = await bscValidatorSet.isWorkingValidator(index)
-    log(index.toNumber(), 'extraInfo 0x3f58fb8ec2f78b65a9077cd3c33b90f3e2332d95:', extraInfo, 'info:', info, isWorking)
-
-
-/*    index = await bscValidatorSet.getCurrentValidatorIndex('0xf8CB6E47a96aCDAe9157276e906E28B5a20c8421')
-    info = await bscValidatorSet.currentValidatorSet(index)
-    extraInfo = await bscValidatorSet.validatorExtraSet(index)
-    isWorking = await bscValidatorSet.isWorkingValidator(index)
-    log(index.toNumber(), 'extraInfo 0xf8CB6E47a96aCDAe9157276e906E28B5a20c8421:', extraInfo, 'info:', info, isWorking)*/
+    const workingValidatorCount = await bscValidatorSet.getWorkingValidatorCount();
+    log('workingValidatorCount:', workingValidatorCount);
 };
 
 const main = async () => {
