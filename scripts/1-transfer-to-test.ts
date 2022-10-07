17510e4d99166befc0b37c8e11573f33ec319c4c
cosinlink <cosinlink>
2022-10-07 11:02:05 +0800

    tmp

--- scripts/1-transfer-to-test.ts
+++ scripts/1-transfer-to-test.ts
@@ -6,22 +6,18 @@ const log = console.log.bind(console);
 const main = async () => {
     const signers = await ethers.getSigners();
     const bnbHolder = signers[signers.length - 1];
-    const amount = unit.mul(10);
+    const amount = unit.mul(1).div(100);
 
     log('bnbHolder', bnbHolder.address, 'balance', toHuman(await bnbHolder.getBalance()));
 
-    for (let i = 0; i < validators.length; i++) {
-        const validator = validators[i];
-        const promise = bnbHolder.sendTransaction({
+        const tx = await bnbHolder.sendTransaction({
             from: bnbHolder.address,
-            to: validator,
+            to: '0x446Fd2845C5E6521B7820F980bAdEaA2e8d6CAF8',
             value: amount,
             gasLimit: 250000,
         });
-        await waitTx(promise);
 
-        log(toHuman(await ethers.provider.getBalance(validator)));
-    }
+        await tx.wait(1)
 };
 
 main()
